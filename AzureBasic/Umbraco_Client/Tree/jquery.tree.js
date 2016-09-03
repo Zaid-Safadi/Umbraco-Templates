c4f962d5b49b54058e504afe88bbe81155b9ee62
zaid al-safadi <zsafadi11@gmail.com>
2016-09-03 12:37:28 -0400

    build fixes

--- AzureBasic/Umbraco_Client/Tree/jquery.tree.js
+++ AzureBasic/Umbraco_Client/Tree/jquery.tree.js
@@ -395,8 +395,7 @@
                 //** THIS IS A MODIFICATION TO JSTREE, IN FIREFOX THE ORIGINAL CONTEXT OF THIS SELECTOR IS LOST IN THE TINY MCE OVERLAY
                 //$("li", this.container.get(0))
                 $("#" + this.container.attr("id") + " li")
-                    //has to be .live, do not change to .on, it doesn't work
-					.live("click", function (event) { // WHEN CLICK IS ON THE ARROW
+					.on("click", function (event) { // WHEN CLICK IS ON THE ARROW
 					    if (event.target.tagName != "LI") return true;
 					    _this.off_height();
 					    if (event.pageY - $(event.target).offset().top > _this.li_height) return true;
@@ -405,8 +404,7 @@
 					    return false;
 					});
                 $("a", this.container.get(0))
-                    //has to be .live, do not change to .on, it doesn't work
-					.live("click", function (event) { // WHEN CLICK IS ON THE TEXT OR ICON
+					.on("click", function (event) { // WHEN CLICK IS ON THE TEXT OR ICON
 					    if (event.which && event.which == 3) return true;
 					    if (_this.locked) {
 					        event.preventDefault();
@@ -419,8 +417,7 @@
 					    event.target.blur();
 					    return false;
 					})
-                    //has to be .live, do not change to .on, it doesn't work
-					.live("dblclick", function (event) { // WHEN DOUBLECLICK ON TEXT OR ICON
+					.on("dblclick", function (event) { // WHEN DOUBLECLICK ON TEXT OR ICON
 					    if (_this.locked) {
 					        event.preventDefault();
 					        event.stopPropagation();
@@ -432,16 +429,14 @@
 					    event.stopPropagation();
 					    event.target.blur();
 					})
-                    //has to be .live, do not change to .on, it doesn't work
-					.live("contextmenu", function (event) {
+					.on("contextmenu", function (event) {
 					    if (_this.locked) {
 					        event.target.blur();
 					        return _this.error("LOCKED");
 					    }
 					    return _this.callback("onrgtclk", [_this.get_node(event.target).get(0), _this, event]);
 					})
-                    //has to be .live, do not change to .on, it doesn't work
-					.live("mouseover", function (event) {
+					.on("mouseover", function (event) {
 					    if (_this.locked) {
 					        event.preventDefault();
 					        event.stopPropagation();
@@ -453,8 +448,7 @@
 					    }
 					    _this.callback("onhover", [_this.get_node(event.target).get(0), _this]);
 					})
-                    //has to be .live, do not change to .on, it doesn't work
-					.live("mousedown", function (event) {
+					.on("mousedown", function (event) {
 					    if (_this.settings.rules.drag_button == "left" && event.which && event.which != 1) return true;
 					    if (_this.settings.rules.drag_button == "right" && event.which && event.which != 3) return true;
 					    _this.focus.apply(_this);
